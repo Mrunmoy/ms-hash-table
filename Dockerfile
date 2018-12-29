e0de13875e670a1b982ff869bdf850864c1454db
Mrunmoy Samal <mrunmoy@gmail.com>
2018-12-29 23:15:45 +1100

    docket build wip

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-# Use an official gcc image as base
-FROM gcc:7.3
-
-# Install deps
-RUN apt-get update && apt-get install -y \
-    gcovr \
-    cmake
---
+++ scripts/config.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Reference: https://codereview.stackexchange.com/questions/137877/shell-script-wrapper-for-docker-build-and-run
+
+CURRENT_USER=$(whoami)
+CURRENT_UID=$(id -u):$(id -g)
+
+readonly DOCKER_IMAGE_NAME="$(CURRENT_USER)/hash-table"
+readonly DOCKER_IMAGE_TAG="0.1.0"
+readonly DOCKER_IMAGE_MOUNT="~/docker/data"
+
+readonly DOCKER_LOCAL_SOURCE_DIRECTORY="docker"
+readonly DOCKER_LOCAL_MOUNT_DIRECTORY="data"
