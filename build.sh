42e2eace4681094316a9d0c3b1e66a2bb8eab8fb
Mrunmoy Samal <mrunmoy@gmail.com>
2018-12-30 12:22:45 +1100

    moved submodule

--- build.sh
+++ build.sh
@@ -2,32 +2,13 @@
 
 # Reference: https://codereview.stackexchange.com/questions/137877/shell-script-wrapper-for-docker-build-and-run
 
-readonly CURRENT_DIRECTORY="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-source "${CURRENT_DIRECTORY}/scripts/config.sh"
-source "${CURRENT_DIRECTORY}/scripts/util.sh"
+# Build & Run Unit Tests
+printf "\n--- Build & Run Unit Tests\n"
 
+cmake . -DCMAKE_BUILD_TYPE=Coverage || echo "failed to configure project"
+make -j 4 || echo"build failed"
 
-build_docker_image() {
-  # Generate image name
-  local name="${DOCKER_IMAGE_NAME}"
-  print "--Building Docker Container ${name}"
-
-  # Run docker with the provided arguments
-  docker build -t "${name}" \
-                  "${CURRENT_DIRECTORY}/${DOCKER_LOCAL_SOURCE_DIRECTORY}"
-  docker run -t "${name}" ./build.sh
-}
-
-main() {
-  # Build & Run Unit Tests
-  printf "\n--- Build & Run Unit Tests\n"
-
-
-  cmake . -DCMAKE_BUILD_TYPE=Coverage || error "failed to configure project"
-  make coverage -j || error "build failed"
-
-  pushd build/bin
-  ./unit-tests  || error "unit test(s) failed"
-  popd
-}
+pushd build/bin
+./unit-tests  || error "unit test(s) failed"
+popd
 
